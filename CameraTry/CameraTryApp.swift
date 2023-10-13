7282839e6a19a0bb29c57da64a8ab5da4595eee3
vindobangun8 <vindobangun8@gmail.com>
2023-10-13 10:54:41 +0700

    first commit

---
+++ CameraTry/CameraTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTryApp.swift
+//  CameraTry
+//
+//  Created by Datita Devindo Bahana on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
